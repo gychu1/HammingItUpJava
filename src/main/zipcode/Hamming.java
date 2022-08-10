24da93b88c3a97791548497da1b9f5288130d4e2
Git-Yee <git-yee@ZipcodersMBP58.fios-router.home>
2022-08-09 21:58:00 -0400

    finished lab. tests passed.

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,25 @@
-package zipcode;
+
 
 public class Hamming {
-    public Hamming(String s, String s1) {
+
+    String s;
+    String s1;
+
+    public Hamming(String s, String s1) throws IllegalArgumentException {
+        if (s.length() != s1.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+        this.s = s;
+        this.s1 = s1;
     }
 
     public int getHammingDistance() {
-        return -1;
+        int counter =0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != s1.charAt(i)) {
+                counter++;
+            }
+        }
+        return counter;
     }
 }
